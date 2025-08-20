3c46a19ae8020bbcf29f2f501e8fd855e41db466
Arman Mir <mirarman8583@gmail.com>
2025-08-20 16:05:30 +0600

    options to event handle

--- js/events.js
+++ js/events.js
@@ -1,1 +1,28 @@
-console.log('External-File');
+console.log("External-File");
+
+//option two to handle event
+function makeYellow() {
+  document.body.style.backgroundColor = "Yellow";
+}
+
+function makeRed() {
+  document.body.style.backgroundColor = "Red";
+}
+
+//option three: get element by id and then set onclick
+const btnMakeBlue = document.getElementById("btn-make-blue");
+// console.log(btnMakeBlue);
+
+btnMakeBlue.onclick = function btnMakeBlue() {
+  document.body.style.backgroundColor = "blue";
+};
+
+
+//Option Three Differce Version
+const btnMakePurple = document.getElementById('btn-make-purple');
+        btnMakePurple.onclick = makePurple;
+
+        function makePurple(){
+            document.body.style.backgroundColor = 'purple';
+        }
+        
