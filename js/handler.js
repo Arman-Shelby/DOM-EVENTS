3672dc7ad6e1d092190ec53f540b5e19b832e5fd
Arman Mir <mirarman8583@gmail.com>
2025-08-20 18:27:59 +0600

    innerText update handler

---
+++ js/handler.js
@@ -0,0 +1,14 @@
+// console.log('handler file added');
+
+document
+  .getElementById("btn-update-title")
+  .addEventListener("click", function () {
+    const pageTitleElement = document.getElementById("page-title");
+    console.log(pageTitleElement);
+    pageTitleElement.innerText = "updated page title text";
+  });
+
+document.getElementById("btn-login").addEventListener("click", function () {
+  const userInfoEl = document.getElementById("user-info");
+  userInfoEl.innerText = "User Logged In Successfully";
+});
